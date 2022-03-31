26e93095171328637ee1c17ad07a617783d92393
StianM <stian.myklebostad@schibsted.com>
2022-03-31 15:53:54 +0200

    feat: Add routing utils to package export

--- packages/extra/src/index.ts
+++ packages/extra/src/index.ts
@@ -3,14 +3,25 @@ import ui_createStyle from './style'
 import ui_createGlobalStyle from './globalStyle'
 import ui_createTransformState from './transformState'
 
+import ui_tokenize from './router/tokenize'
+import ui_createDecisionTree from './router/decisionTree'
+import ui_resolveRoute from './router/resolveRoute'
+
 export const createHashRouter = ui_createHashRouter
 export const createStyle = ui_createStyle
 export const createGlobalStyle = ui_createGlobalStyle
 export const createTransformState = ui_createTransformState
+
+export const routing = {
+  tokenize: ui_tokenize,
+  createDecisionTree: ui_createDecisionTree,
+  resolveRoute: ui_resolveRoute
+}
 
 export default {
   createHashRouter,
   createStyle,
   createGlobalStyle,
   createTransformState,
+  routing,
 }
